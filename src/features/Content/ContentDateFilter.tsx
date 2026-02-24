f44e8351035866ec0803dfcec354644fcf89dc04
raigomes <raigomes2@hotmail.com>
2026-02-23 21:02:53 -0300

    Add ContentDateFilter component logic

--- src/features/Content/ContentDateFilter.tsx
+++ src/features/Content/ContentDateFilter.tsx
@@ -1,13 +1,27 @@
 import React from "react";
 import Button from "../../components/Button";
+import { useVendas } from "../../context/VendasContext";
+import { getFirstAndLastDayOfMonth, getLastFourMonths } from "../../utils/date";
 
 const ContentDateFilter = () => {
+  const { setInicio, setFim } = useVendas();
+  const today = new Date().toISOString();
+  const months = getLastFourMonths(today);
+
+  function handleClick(date: Date) {
+    const { firstDay, lastDay } = getFirstAndLastDayOfMonth(date);
+
+    setInicio(firstDay);
+    setFim(lastDay);
+  }
+
   return (
     <div className="flex">
-      <Button>novembro</Button>
-      <Button>dezembro</Button>
-      <Button>janeiro</Button>
-      <Button>fevereiro</Button>
+      {months.map(({ fullMonth, date }) => (
+        <Button key={fullMonth} onClick={() => handleClick(date)}>
+          {fullMonth}
+        </Button>
+      ))}
     </div>
   );
 };
--- src/utils/date.ts
+++ src/utils/date.ts
@@ -8,3 +8,36 @@ export function getISODate(
     dateInMiliseconds - subtractDays * millisecondsPerDay;
   return new Date(newDateInMilliseconds).toISOString().split("T")[0];
 }
+
+function getFullMonth(date: Date) {
+  const nomeMes = date.toLocaleString("pt-BR", { month: "long" });
+  return nomeMes;
+}
+
+export function getLastFourMonths(isoDate: string) {
+  const date = new Date(isoDate);
+  const month = date.getMonth();
+  const array = [];
+
+  for (let i = month - 3; i <= month; i++) {
+    const newDate = new Date(date.getTime());
+    newDate.setMonth(i);
+    array.push({
+      fullMonth: getFullMonth(newDate),
+      date: newDate,
+    });
+  }
+
+  return array;
+}
+
+export function getFirstAndLastDayOfMonth(date: Date) {
+  const firstDay = getISODate(
+    new Date(date.getFullYear(), date.getMonth(), 1).getTime(),
+  );
+  const lastDay = getISODate(
+    new Date(date.getFullYear(), date.getMonth() + 1, 0).getTime(),
+  );
+
+  return { firstDay, lastDay };
+}
